d7962d88d23118d5409da90aa64cbbf784492d68
Kevin Bradwick <kevinbradwick@gmail.com>
2023-04-10 09:37:44 +0100

    wip

--- src/age-calculator-app/script.mjs
+++ src/age-calculator-app/script.mjs
@@ -4,19 +4,36 @@ document.querySelector("form").addEventListener("submit", (event) => {
   const day = document.querySelector("#day");
   const month = document.querySelector("#month");
   const year = document.querySelector("#year");
+  const outputYears = document.querySelector("#output-years");
+  const outputMonths = document.querySelector("#output-months");
+  const outputDays = document.querySelector("#output-days");
 
   const container = document.querySelector("#input-fields");
   const invalidMessage = document.querySelector("#invalid-message");
 
-  // validate day
-  if (!day.value || day.value < 1 || day.value > 31) {
-    container.classList.add("error");
-    return;
-  }
-
   // remove previous error message
   container.classList.remove("error");
   invalidMessage.classList.add("hidden");
+  container.querySelectorAll("p").forEach((e) => e.classList.add("hidden"));
+
+  // reset counter
+  outputYears.textContent = "--";
+  outputMonths.textContent = "--";
+  outputDays.textContent = "--";
+
+  // validate day
+  if (!day.value || day.value < 1 || day.value > 31) {
+    container.classList.add("error");
+    day.nextElementSibling.classList.remove("hidden");
+    return;
+  }
+
+  // validate month
+  if (!month.value || month.value < 1 || month.value > 12) {
+    container.classList.add("error");
+    month.nextElementSibling.classList.remove("hidden");
+    return;
+  }
 
   // create a new Date object with the user's input
   const birthDate = new Date(year.value, month.value - 1, day.value);
@@ -28,10 +45,15 @@ document.querySelector("form").addEventListener("submit", (event) => {
     return;
   }
 
-  // calculate how many years old the user is
-  const years = new Date().getFullYear() - birthDate.getFullYear();
-  // calculate how many months old the user is
-  const months = new Date().getMonth() - birthDate.getMonth();
-  // calculate how many days old the user is
-  const days = new Date().getDate() - birthDate.getDate();
+  const diff = Math.floor(now - birthDate);
+  const dayMs = 1000 * 60 * 60 * 24;
+
+  const totalDays = Math.floor(diff / dayMs);
+  const yearsOld = Math.floor(totalDays / 365);
+  const monthsOld = Math.floor((totalDays % 365) / 30);
+  const daysOld = Math.floor(totalDays - (yearsOld * 365) - (monthsOld * 30));
+
+  outputYears.textContent = yearsOld;
+  outputMonths.textContent = monthsOld;
+  outputDays.textContent = daysOld;
 });
